80dfcb3b1454866e63aa192997d83f446818a0dd
iamAbduley <miniyazilimci@gmail.com>
2023-08-17 23:13:08 +0300

    Renamed points

--- cogs/utils/functions.py
+++ cogs/utils/functions.py
@@ -5,6 +5,7 @@
  * For more information, see README.md and LICENSE
 """
 from cogs.utils.constants import Emojis
+from cogs.utils.database.fetchdata import create_career_data
 
 enought_balance = Emojis.enought_balance
 
@@ -15,3 +16,24 @@ async def balance_check(interaction, user_cash, required_amount):
             content = f"{enought_balance} Yeterli bakiyeniz bulunmuyor! {user_cash-required_amount:,} LiCash eksik!",
             ephemeral = True)
     
+async def add_xp(client, _id, xp_category):
+    xp_types = [
+        "fisher_xp",   # 0
+        "hunter_xp",   # 1
+        "miner_xp",    # 2
+        "forester_xp", # 3
+        "send_xp",     # 4
+        "gamble_xp"    # 5
+        ]
+    
+    if (xp_category not in xp_types) or (5 < xp_category < 0):
+        raise KeyError("Please enter a valid category! (0-5)")
+    
+    if isinstance(int, xp_category):
+        xp_category = xp_types[xp_category]
+
+    user_data, collection = await create_career_data(client, _id)
+    user_data["xp"][xp_category] += 2
+    
+    await collection.replace_one({"_id": _id}, user_data)
+    print(f"Added XP to ({_id})")
