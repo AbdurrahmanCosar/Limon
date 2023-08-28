ff615f8439d0500d35d5c5f555dec2cb8e3cb78b
iamAbduley <miniyazilimci@gmail.com>
2023-08-29 02:27:48 +0300

    Added LICENSE texts

--- cogs/commands/general/user_info.py
+++ cogs/commands/general/user_info.py
@@ -1,3 +1,10 @@
+"""
+ * Limon Bot for Discord
+ * Copyright (C) 2022 AbdurrahmanCosar
+ * This software is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ * For more information, see README.md and LICENSE
+"""
+
 from discord import app_commands, Interaction, Member, File
 from discord.ext import commands
 from io import BytesIO
--- cogs/utils/DrawImage/Draw/user_info.py
+++ cogs/utils/DrawImage/Draw/user_info.py
@@ -1,3 +1,10 @@
+"""
+ * Limon Bot for Discord
+ * Copyright (C) 2022 AbdurrahmanCosar
+ * This software is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ * For more information, see README.md and LICENSE
+"""
+
 from PIL import Image, ImageChops, ImageDraw, ImageFont
 from ...database.fetchdata import create_career_data, create_wallet
 from ..functions import Functions
