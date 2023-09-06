c1136f816dcb284e592eb1dd4f2bf173d6efa61a
iamAbduley <miniyazilimci@gmail.com>
2023-09-06 18:56:57 +0300

    Add LICENSE text

--- cogs/utils/DrawImage/Draw/career_ui.py
+++ cogs/utils/DrawImage/Draw/career_ui.py
@@ -1,3 +1,9 @@
+"""
+ * Limon Bot for Discord
+ * Copyright (C) 2022 AbdurrahmanCosar
+ * This software is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ * For more information, see README.md and LICENSE
+"""
 from PIL import Image, ImageDraw, ImageFont
 from discord import Interaction
 from cogs.utils.DrawImage.assets import Assets
--- cogs/utils/buttons.py
+++ cogs/utils/buttons.py
@@ -1,3 +1,9 @@
+"""
+ * Limon Bot for Discord
+ * Copyright (C) 2022 AbdurrahmanCosar
+ * This software is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ * For more information, see README.md and LICENSE
+"""
 from discord import Interaction, ButtonStyle, ui
 from cogs.utils.constants import Emojis
 from typing import Optional
