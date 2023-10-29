b4a18935dc5747141644c21f2d664f6b71e05ee8
iamAbduley <miniyazilimci@gmail.com>
2023-10-29 12:35:15 +0300

    Changed daily money system and deleted tasks folder

--- cogs/commands/eco/dailyMoney.py
+++ cogs/commands/eco/dailyMoney.py
@@ -10,7 +10,7 @@ from discord.ext import commands
 from cogs.utils.cooldown import set_cooldown
 from cogs.utils.constants import Emojis
 from cogs.utils.database.fetchdata import create_wallet
-
+from random import randint
 
 wallet = Emojis.limonbank
 morelicash = Emojis.morelicash
@@ -25,19 +25,19 @@ class DailyMoney(commands.Cog):
             description="Günlük LiCash'inizi alın.",
             extras={
                 'category': 'eco',
-                'help': "Hesabınıza yatan günlü LiCash'inizi alın.'"
+                'help': "Hesabınıza yatan günlü LiCash'inizi alın."
             })
     @app_commands.checks.dynamic_cooldown(set_cooldown(60))
     async def daily_money(self, interaction: Interaction):
         user_data, collection = await create_wallet(self.bot, interaction.user.id)
         
-        accumulated_money = user_data["accumulated_money"]
-        await interaction.response.send_message(content = f"{morelicash} Günlük kazancınız **{accumulated_money:,}LC**")
-
-        user_data["cash"] += accumulated_money
-        user_data["accumulated_money"] = 0
-
+        daily_cash = randint(500, 7500)
+        user_data['cash'] += daily_cash
+        
         await collection.replace_one({"_id": interaction.user.id}, user_data)
+        await interaction.response.send_message(
+                content = f"{morelicash} Günlük Kazancınız: {daily_cash:,} LiCash")
+        
         
 
 async def setup(bot: commands.Bot):
